f97cb4cdca51825564f9bf550d8027bd16de712a
Amol Grover <29180487+frextrite@users.noreply.github.com>
2019-06-02 13:10:19 +0530

    SourceRange.java: Complete class
    Add items and getters

--- src/main/java/io/coala/jetbrains/utils/SourceRange.java
+++ src/main/java/io/coala/jetbrains/utils/SourceRange.java
@@ -1,4 +1,19 @@
 package io.coala.jetbrains.utils;
 
 public class SourceRange {
+    private final String file;
+    private final int line;
+    private final int column;
+
+    public SourceRange(String file, int line, int column) {
+        this.file = file;
+        this.line = line;
+        this.column = column;
+    }
+
+    public String getFile() { return file; }
+
+    public int getLine() { return line; }
+
+    public int getColumn() { return column; }
 }

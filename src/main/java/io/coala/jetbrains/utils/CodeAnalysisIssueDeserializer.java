5225210cfaae89910bdcefc63bfb48dd5027840a
Amol Grover <29180487+frextrite@users.noreply.github.com>
2019-06-03 22:56:19 +0530

    CodeAnalysisIssueDeserializer.java: Add deserialization code
    This class deserializes JSON data into a list of issue objects

---
+++ src/main/java/io/coala/jetbrains/utils/CodeAnalysisIssueDeserializer.java
@@ -0,0 +1,65 @@
+package io.coala.jetbrains.utils;
+
+import gherkin.deps.com.google.gson.*;
+import gherkin.deps.com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class CodeAnalysisIssueDeserializer {
+    /**
+     * This method deserializes raw JSON output from coala analysis
+     * and converts it to Java friendly objects
+     *
+     * @param jsonString the String to be deserialized
+     * @return list of {@link CodeAnalysisIssue} objects
+     */
+    public static List<CodeAnalysisIssue> getAllCodeAnalysisIssues(String jsonString) {
+        final GsonBuilder gsonBuilder = new GsonBuilder();
+        gsonBuilder.registerTypeAdapter(List.class, new ExtractCodeAnalysisIssuesFromJson());
+        Type typeOfListOfCodeAnalysisIssues = new TypeToken<List<CodeAnalysisIssue>>() {}.getType();
+        return gsonBuilder.create().fromJson(jsonString, typeOfListOfCodeAnalysisIssues);
+    }
+
+    /**
+     * This method creates a new issue object from
+     * the corresponding JsonElement
+     *
+     * @param jsonElement
+     * @return new {@link CodeAnalysisIssue} object synthesized from JSON
+     */
+    public static CodeAnalysisIssue getNewCodeAnalysisIssue(JsonElement jsonElement) {
+        final GsonBuilder gsonBuilder = new GsonBuilder();
+        gsonBuilder.registerTypeAdapter(CodeAnalysisIssue.class, new DeserializeIssueFromJson());
+        return gsonBuilder.create().fromJson(jsonElement, CodeAnalysisIssue.class);
+    }
+
+    /**
+     * Custom deserializer class for deserializing JSON output from coala analysis
+     * <p>
+     * Currently, a single list of issue objects is compiled from
+     * all the different sections in .coafile
+     * @TODO The section information may be useful in further versions of the plugin
+     */
+    private static class ExtractCodeAnalysisIssuesFromJson implements JsonDeserializer<List<CodeAnalysisIssue>> {
+        @Override
+        public List<CodeAnalysisIssue> deserialize(JsonElement jsonElement, Type type, JsonDeserializationContext jsonDeserializationContext) throws JsonParseException {
+            final JsonObject jsonObject = jsonElement.getAsJsonObject();
+            final JsonObject jsonObjectResults = jsonObject.get("results").getAsJsonObject();
+
+            List<CodeAnalysisIssue> codeAnalysisIssueList = new ArrayList<>();
+
+            for (Map.Entry<String, JsonElement> section : jsonObjectResults.entrySet()) {
+                final JsonElement sectionValue = section.getValue();
+                final JsonArray sectionIssueList = sectionValue.getAsJsonArray();
+                for (JsonElement sectionIssue  : sectionIssueList) {
+                    codeAnalysisIssueList.add(getNewCodeAnalysisIssue(sectionIssue));
+                }
+            }
+
+            return codeAnalysisIssueList;
+        }
+    }
+}

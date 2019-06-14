9e05a62dbe9a356e677e706ff91315b8b0f7c75d
Amol Grover <29180487+frextrite@users.noreply.github.com>
2019-06-15 00:52:26 +0530

    process

--- src/main/java/io/coala/jetbrains/CodeAnalysisAction.java
+++ src/main/java/io/coala/jetbrains/CodeAnalysisAction.java
@@ -1,9 +1,13 @@
 package io.coala.jetbrains;
 
+import com.intellij.execution.ExecutionException;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.application.Application;
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.diagnostic.Logger;
-import com.intellij.openapi.project.DumbAware;
+import com.intellij.openapi.project.Project;
+import io.coala.jetbrains.utils.CodeAnalysisRunner;
 import org.jetbrains.annotations.NotNull;
 
 public class CodeAnalysisAction extends AnAction {
@@ -16,6 +20,59 @@ public class CodeAnalysisAction extends AnAction {
 
     @Override
     public void actionPerformed(AnActionEvent e) {
-        // TODO: insert action logic here
+        final Project project = e.getProject();
+        final String projectBasePath = project != null ? project.getBasePath() : null;
+
+        if(project == null || projectBasePath == null) {
+            LOGGER.debug("Project not opened. Exiting AnAction()");
+            return;
+        }
+
+        final CodeAnalysisRunner codeAnalysisRunner = project.getComponent(CodeAnalysisRunner.class);
+
+        final Application app = ApplicationManager.getApplication();
+
+        /*app.executeOnPooledThread( () -> {
+            try {
+                LOGGER.warn("sleeping!");
+                Thread.sleep(30000);
+                LOGGER.warn("waking");
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
+            }
+        });*/
+
+
+        /*app.invokeLater(() -> app.executeOnPooledThread(() -> {
+            try {
+                codeAnalysisRunner.submit(projectBasePath, "/home/amol/anaconda3/envs/coala/bin/coala", "jetbrains");
+            } catch (ExecutionException ex) {
+                ex.printStackTrace();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
+            }
+        }));*/
+
+
+        try {
+            codeAnalysisRunner.submit(projectBasePath, "/home/amol/anaconda3/envs/coala/bin/coala", "jetbrains");
+        } catch (ExecutionException ex) {
+            ex.printStackTrace();
+        }
+
+        /*app.executeOnPooledThread(() -> {
+            try {
+                codeAnalysisRunner.submit(projectBasePath, "/home/amol/anaconda3/envs/coala/bin/coala", "jetbrains");
+            } catch (ExecutionException ex) {
+                ex.printStackTrace();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
+            }
+        });*/
+
     }
 }
--- src/main/java/io/coala/jetbrains/utils/CodeAnalysisRunner.java
+++ src/main/java/io/coala/jetbrains/utils/CodeAnalysisRunner.java
@@ -2,11 +2,23 @@ package io.coala.jetbrains.utils;
 
 import com.intellij.execution.ExecutionException;
 import com.intellij.execution.configurations.GeneralCommandLine;
-import com.intellij.execution.process.OSProcessHandler;
+import com.intellij.execution.process.*;
+import com.intellij.openapi.components.ProjectComponent;
+import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.Key;
+import org.jetbrains.annotations.NotNull;
 
 import javax.annotation.Nonnull;
 
-public class CodeAnalysisRunner {
+public class CodeAnalysisRunner implements ProjectComponent {
+    private static Logger LOGGER = Logger.getInstance(CodeAnalysisRunner.class);
+    private final Project project;
+
+    public CodeAnalysisRunner(Project project) {
+        this.project = project;
+    }
+
     /**
      * This method creates a runnable process and runs it.
      * {@link OSProcessHandler#startNotify()} is used to capture the output
@@ -16,14 +28,46 @@ public class CodeAnalysisRunner {
      * @param section the coala section tag to run analysis on
      * @throws ExecutionException
      */
-    public static void submit(@Nonnull String cwd, @Nonnull String executable, @Nonnull String section) throws ExecutionException {
+    public void submit(@Nonnull String cwd, @Nonnull String executable, @Nonnull String section) throws ExecutionException {
         final GeneralCommandLine commandLine = getNewGeneralCommandLine(cwd, executable, section);
+
         final String commandLineString = commandLine.getCommandLineString();
         final Process process = commandLine.createProcess();
 
+        LOGGER.warn("Starting process");
+        LOGGER.warn(commandLine.getCommandLineString());
+        LOGGER.warn("in " + commandLine.getWorkDirectory());
+
         final OSProcessHandler processHandler = new OSProcessHandler(process, commandLineString);
 
+        final ProcessOutput processOutput = new ProcessOutput();
+
+        processHandler.addProcessListener(new ProcessAdapter() {
+            @Override
+            public void onTextAvailable(@NotNull ProcessEvent event, @NotNull Key outputType) {
+                super.onTextAvailable(event, outputType);
+                if(outputType.equals(ProcessOutputTypes.STDERR)) {
+                    processOutput.appendStderr(event.getText());
+                } else if(!outputType.equals(ProcessOutputTypes.SYSTEM)) {
+                    processOutput.appendStdout(event.getText());
+                }
+            }
+        });
+
         processHandler.startNotify();
+
+        // TODO: add wait logic
+        processHandler.waitFor(30000);
+
+
+        // TODO: handle stdout and stderr
+        final String stdout = processOutput.getStdout();
+        final String stderr = processOutput.getStderr();
+
+        LOGGER.warn(stdout);
+        LOGGER.warn(stderr);
+
+        LOGGER.warn("Done!");
     }
 
     /**
@@ -43,7 +87,7 @@ public class CodeAnalysisRunner {
      * @param section the coala section tag to run analysis on
      * @return an instance of GeneralCommandLine
      */
-    public static GeneralCommandLine getNewGeneralCommandLine(@Nonnull String cwd, @Nonnull String executable, @Nonnull String section) {
+    public GeneralCommandLine getNewGeneralCommandLine(@Nonnull String cwd, @Nonnull String executable, @Nonnull String section) {
         final GeneralCommandLine commandLine = new GeneralCommandLine();
         commandLine.setWorkDirectory(cwd);
         commandLine.setExePath(executable);

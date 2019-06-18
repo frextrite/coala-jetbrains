98ef52801f9375e1a1e0ce4236a1913d9465d55d
Amol Grover <29180487+frextrite@users.noreply.github.com>
2019-06-18 17:24:25 +0530

    CodeAnalysisExecutor.java: Add class for executing process
    Uses built in executor and progress manager classe
    provided by OpenAPI for running a custom task efficiently

---
+++ src/main/java/io/coala/jetbrains/analysis/CodeAnalysisExecutor.java
@@ -0,0 +1,25 @@
+package io.coala.jetbrains.analysis;
+
+import com.intellij.openapi.components.ProjectComponent;
+import com.intellij.openapi.progress.ProgressManager;
+import com.intellij.openapi.progress.impl.BackgroundableProcessIndicator;
+import com.intellij.openapi.project.Project;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class CodeAnalysisExecutor implements ProjectComponent {
+    private final ExecutorService executor = Executors.newSingleThreadExecutor();
+    private final ProgressManager progressManager;
+    private final Project project;
+
+    CodeAnalysisExecutor(Project project, ProgressManager progressManager) {
+        this.project = project;
+        this.progressManager = progressManager;
+    }
+
+    public void runTask(CodeAnalysisTask task) {
+        BackgroundableProcessIndicator processIndicator = new BackgroundableProcessIndicator(task);
+        executor.submit(() -> progressManager.runProcess(() ->task.run(processIndicator), processIndicator));
+    }
+}

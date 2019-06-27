02d45cdc839094ffb6d85865433e735434ae19bc
Amol Grover <29180487+frextrite@users.noreply.github.com>
2019-06-27 23:44:16 +0530

    Notifier.java: Update methods to be more complete and user friendly

--- src/main/java/io/coala/jetbrains/utils/Notifier.java
+++ src/main/java/io/coala/jetbrains/utils/Notifier.java
@@ -1,14 +1,16 @@
 package io.coala.jetbrains.utils;
 
-import com.intellij.notification.Notification;
-import com.intellij.notification.NotificationDisplayType;
-import com.intellij.notification.NotificationGroup;
-import com.intellij.notification.Notifications;
+import com.intellij.notification.*;
+import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.ui.MessageType;
+import com.intellij.openapi.wm.ToolWindowManager;
+import org.apache.commons.lang.exception.ExceptionUtils;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 public final class Notifier {
-    private static final String prefix = "coala: ";
+    private static final String title = "coala";
     private static final NotificationGroup NOTIFICATION = new NotificationGroup("coala", NotificationDisplayType.BALLOON, true);
 
     public static void showNotification(Notification notification) {
@@ -16,17 +18,28 @@ public final class Notifier {
     }
 
     public static void showInformationNotification(String message) {
-        final Notification notification = NOTIFICATION.createNotification(prefix + message, MessageType.INFO);
+        final Notification notification = NOTIFICATION.createNotification(title, message, NotificationType.INFORMATION, null);
         showNotification(notification);
     }
 
     public static void showWarningNotification(String message) {
-        final Notification notification = NOTIFICATION.createNotification(prefix + message, MessageType.WARNING);
+        final Notification notification = NOTIFICATION.createNotification(title, message, NotificationType.WARNING, null);
         showNotification(notification);
     }
 
-    public static void showErrorNotification(String message) {
-        final Notification notification = NOTIFICATION.createNotification(prefix + message, MessageType.ERROR);
+    public static void showErrorNotification(String message, @Nullable Throwable throwable) {
+        final Notification notification = NOTIFICATION.createNotification(title,
+                message + "<br/>" + ExceptionUtils.getFullStackTrace(throwable),
+                NotificationType.ERROR,
+                new NotificationListener.UrlOpeningListener(true))
+                .addAction(new NotificationAction("more") {
+                    @Override
+                    public void actionPerformed(@NotNull AnActionEvent anActionEvent, @NotNull Notification notification) {
+                        ApplicationManager.getApplication().invokeLater(() -> {
+                            ToolWindowManager.getInstance(anActionEvent.getProject()).getToolWindow("Event Log").show(null);
+                        });
+                    }
+                });
         showNotification(notification);
     }
 }

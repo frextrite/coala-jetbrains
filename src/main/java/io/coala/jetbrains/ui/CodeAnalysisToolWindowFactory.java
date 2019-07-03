ceadfade53716d3d1eb0730b600c944003c1989d
Amol Grover <29180487+frextrite@users.noreply.github.com>
2019-07-03 23:03:10 +0530

    CodeAnalysisToolWindowFactory.java: Create tool window
    This tool window holds the main coala console

---
+++ src/main/java/io/coala/jetbrains/ui/CodeAnalysisToolWindowFactory.java
@@ -0,0 +1,21 @@
+package io.coala.jetbrains.ui;
+
+import com.intellij.execution.ui.ConsoleView;
+import com.intellij.openapi.project.DumbAware;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.wm.ToolWindow;
+import com.intellij.openapi.wm.ToolWindowFactory;
+import com.intellij.ui.content.Content;
+import com.intellij.ui.content.ContentFactory;
+import org.jetbrains.annotations.NotNull;
+
+public class CodeAnalysisToolWindowFactory implements ToolWindowFactory, DumbAware {
+    @Override
+    public void createToolWindowContent(@NotNull Project project, @NotNull ToolWindow toolWindow) {
+        final CodeAnalysisConsoleView codeAnalysisConsoleView = project.getComponent(CodeAnalysisConsoleView.class);
+        final ContentFactory contentFactory = ContentFactory.SERVICE.getInstance();
+        final ConsoleView consoleView = codeAnalysisConsoleView.getConsoleView();
+        final Content content = contentFactory.createContent(consoleView.getComponent(), "Analysis Log", false);
+        toolWindow.getContentManager().addContent(content);
+    }
+}

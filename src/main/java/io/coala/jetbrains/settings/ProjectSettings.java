3661b4d474b9d7805037396e056a1fcfd3eb3323
Amol Grover <29180487+frextrite@users.noreply.github.com>
2019-07-03 23:33:55 +0530

    ProjectSettings.java: Automatically detect coala executable
    coala executable is automatically detected on Unix and Windows
    and corresponding value set in the Settings class

--- src/main/java/io/coala/jetbrains/settings/ProjectSettings.java
+++ src/main/java/io/coala/jetbrains/settings/ProjectSettings.java
@@ -1,29 +1,44 @@
 package io.coala.jetbrains.settings;
 
+import com.intellij.execution.ExecutionException;
+import com.intellij.execution.configurations.GeneralCommandLine;
+import com.intellij.openapi.application.Application;
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.application.ModalityState;
 import com.intellij.openapi.components.ProjectComponent;
+import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.openapi.progress.ProgressManager;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.SystemInfo;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import org.apache.commons.io.IOUtils;
 
 public class ProjectSettings implements ProjectComponent {
 
+  private static Logger LOGGER = Logger.getInstance(ProjectSettings.class);
+
   private final Project project;
+  private final ProgressManager progressManager;
   private long timeOutInSeconds;
   private String cwd;
   private String executable;
   private List<String> sections;
 
-  public ProjectSettings(Project project) {
+
+  public ProjectSettings(Project project, ProgressManager progressManager) {
     this.project = project;
     this.cwd = project.getBasePath();
-    /* TODO: handle other OSes
-     *  in the future this value will be set from the settings panel
-     */
     this.executable = null;
     this.sections = new ArrayList<>();
-    /* TODO: add timeout option in settings panel
-     */
+    /* TODO: add timeout option in settings panel */
     this.timeOutInSeconds = 120;
+    this.progressManager = progressManager;
+    runAutomaticExecutableRetrieval();
   }
 
   public Project getProject() {
@@ -34,6 +49,10 @@ public class ProjectSettings implements ProjectComponent {
     return timeOutInSeconds;
   }
 
+  public void setTimeOutInSeconds(long timeOutInSeconds) {
+    this.timeOutInSeconds = timeOutInSeconds;
+  }
+
   public String getCwd() {
     return cwd;
   }
@@ -42,16 +61,12 @@ public class ProjectSettings implements ProjectComponent {
     return executable;
   }
 
-  public List<String> getSections() {
-    return sections;
-  }
-
-  public void setTimeOutInSeconds(long timeOutInSeconds) {
-    this.timeOutInSeconds = timeOutInSeconds;
-  }
-
   public void setExecutable(String executable) {
     this.executable = executable;
+  }
+
+  public List<String> getSections() {
+    return sections;
   }
 
   public void addSectionsToFilter(List<String> sectionsList) {
@@ -72,5 +87,43 @@ public class ProjectSettings implements ProjectComponent {
 
   public void clearSections() {
     this.sections.clear();
+  }
+
+  public Path determineAndSetExecutable(String executable)
+      throws ExecutionException, InterruptedException, IOException {
+    final GeneralCommandLine commandLine = new GeneralCommandLine().withParameters(executable);
+    if (SystemInfo.isUnix) {
+      commandLine.setExePath("/usr/bin/which");
+    } else if (SystemInfo.isWindows) {
+      commandLine.setExePath("C:\\Windows\\System32\\where.exe");
+    }
+    Process process = commandLine.createProcess();
+    process.waitFor();
+    if (process.exitValue() == 0) {
+      String parsed_executable = IOUtils
+          .toString(process.getInputStream(), Charset.defaultCharset()).trim();
+      return Paths.get(parsed_executable);
+    }
+    return null;
+  }
+
+  private void runAutomaticExecutableRetrieval() {
+    final Application app = ApplicationManager.getApplication();
+    app.executeOnPooledThread(() -> {
+      app.invokeLater(() -> {
+        try {
+          Path coalaExecutableLocation = determineAndSetExecutable("coala");
+          if (coalaExecutableLocation != null) {
+            executable = coalaExecutableLocation.toString();
+          }
+        } catch (ExecutionException e) {
+          e.printStackTrace();
+        } catch (InterruptedException e) {
+          e.printStackTrace();
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      }, ModalityState.NON_MODAL);
+    });
   }
 }

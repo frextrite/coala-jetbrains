41cc124e3129e004e40a456855e08f000d2cf8aa
Amol Grover <29180487+frextrite@users.noreply.github.com>
2019-07-08 14:10:16 +0530

    Notifier.java, CodeAnalysisTask.java: Reformat Code

--- src/main/java/io/coala/jetbrains/utils/Notifier.java
+++ src/main/java/io/coala/jetbrains/utils/Notifier.java
@@ -1,9 +1,14 @@
 package io.coala.jetbrains.utils;
 
-import com.intellij.notification.*;
+import com.intellij.notification.Notification;
+import com.intellij.notification.NotificationAction;
+import com.intellij.notification.NotificationDisplayType;
+import com.intellij.notification.NotificationGroup;
+import com.intellij.notification.NotificationListener.UrlOpeningListener;
+import com.intellij.notification.NotificationType;
+import com.intellij.notification.Notifications;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.ui.MessageType;
 import com.intellij.openapi.wm.ToolWindowManager;
 import org.apache.commons.lang.exception.ExceptionUtils;
 import org.jetbrains.annotations.NotNull;
@@ -11,7 +16,9 @@ import org.jetbrains.annotations.Nullable;
 
 public final class Notifier {
     private static final String title = "coala";
-    private static final NotificationGroup NOTIFICATION = new NotificationGroup("coala", NotificationDisplayType.BALLOON, true);
+    private static final NotificationGroup NOTIFICATION = new NotificationGroup("coala",
+        NotificationDisplayType.BALLOON,
+        true);
 
     public static void showNotification(Notification notification) {
         ApplicationManager.getApplication().invokeLater(() -> Notifications.Bus.notify(notification));
@@ -28,18 +35,26 @@ public final class Notifier {
     }
 
     public static void showErrorNotification(String message, @Nullable Throwable throwable) {
+        final UrlOpeningListener urlOpeningListener = new UrlOpeningListener(true);
+
         final Notification notification = NOTIFICATION.createNotification(title,
                 message + "<br/>" + ExceptionUtils.getFullStackTrace(throwable),
                 NotificationType.ERROR,
-                new NotificationListener.UrlOpeningListener(true))
-                .addAction(new NotificationAction("more") {
-                    @Override
-                    public void actionPerformed(@NotNull AnActionEvent anActionEvent, @NotNull Notification notification) {
-                        ApplicationManager.getApplication().invokeLater(() -> {
-                            ToolWindowManager.getInstance(anActionEvent.getProject()).getToolWindow("Event Log").show(null);
-                        });
-                    }
-                });
+                urlOpeningListener)
+            .addAction(showEventLogToolWindowAction());
+
         showNotification(notification);
+    }
+
+    private static NotificationAction showEventLogToolWindowAction() {
+        String actionText = "more";
+
+        return new NotificationAction(actionText) {
+            @Override
+            public void actionPerformed(@NotNull AnActionEvent anActionEvent, @NotNull Notification notification) {
+                ApplicationManager.getApplication().invokeLater(() ->
+                    ToolWindowManager.getInstance(anActionEvent.getProject()).getToolWindow("Event Log").show(null));
+            }
+        };
     }
 }

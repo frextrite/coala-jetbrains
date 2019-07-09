3e5f65988c343ecd921c32bb767cb7887219a12e
Amol Grover <29180487+frextrite@users.noreply.github.com>
2019-07-09 12:41:31 +0530

    CodeAnalysisConsoleView.java: Reformat code
    according to Google Java standards

--- src/main/java/io/coala/jetbrains/ui/CodeAnalysisConsoleView.java
+++ src/main/java/io/coala/jetbrains/ui/CodeAnalysisConsoleView.java
@@ -5,59 +5,59 @@ import com.intellij.execution.ui.ConsoleView;
 import com.intellij.execution.ui.ConsoleViewContentType;
 import com.intellij.openapi.components.ProjectComponent;
 import com.intellij.openapi.project.Project;
-import io.coala.jetbrains.utils.CodeAnalysisIssue;
 import io.coala.jetbrains.utils.CodeInspectionSeverity;
 
 public class CodeAnalysisConsoleView implements ProjectComponent {
-    private final Project project;
-    private ConsoleView consoleView;
 
-    public CodeAnalysisConsoleView(Project project) {
-        this.project = project;
-        this.consoleView = TextConsoleBuilderFactory.getInstance().createBuilder(project).getConsole();
+  private final Project project;
+  private ConsoleView consoleView;
+
+  public CodeAnalysisConsoleView(Project project) {
+    this.project = project;
+    this.consoleView = TextConsoleBuilderFactory.getInstance().createBuilder(project).getConsole();
+  }
+
+  public ConsoleView getConsoleView() {
+    return consoleView;
+  }
+
+  public void print(String message, CodeInspectionSeverity severity) {
+    switch (severity) {
+      case INFO:
+        info(message);
+        break;
+
+      case WARNING:
+        warn(message);
+        break;
+
+      case ERROR:
+        error(message);
+        break;
+
+      case DEBUG:
+        debug(message);
+        break;
+
+      default:
+        error("Internal Plugin Error. Unidentified severity specified.");
+        break;
     }
+  }
 
-    public ConsoleView getConsoleView() {
-        return consoleView;
-    }
+  private void info(String message) {
+    this.consoleView.print(message + "\n", ConsoleViewContentType.NORMAL_OUTPUT);
+  }
 
-    public void print(String message, CodeInspectionSeverity severity) {
-        switch(severity) {
-            case INFO:
-                info(message);
-                break;
+  private void warn(String message) {
+    this.consoleView.print(message + "\n", ConsoleViewContentType.LOG_WARNING_OUTPUT);
+  }
 
-            case WARNING:
-                warn(message);
-                break;
+  private void error(String message) {
+    this.consoleView.print(message + "\n", ConsoleViewContentType.ERROR_OUTPUT);
+  }
 
-            case ERROR:
-                error(message);
-                break;
-
-            case DEBUG:
-                debug(message);
-                break;
-
-            default:
-                error("Internal Plugin Error. Unidentified severity specified.");
-                break;
-        }
-    }
-
-    private void info(String message) {
-        this.consoleView.print(message + "\n", ConsoleViewContentType.NORMAL_OUTPUT);
-    }
-
-    private void warn(String message) {
-        this.consoleView.print(message + "\n", ConsoleViewContentType.LOG_WARNING_OUTPUT);
-    }
-
-    private void error(String message) {
-        this.consoleView.print(message + "\n", ConsoleViewContentType.ERROR_OUTPUT);
-    }
-
-    private void debug(String message) {
-        this.consoleView.print(message + "\n", ConsoleViewContentType.LOG_DEBUG_OUTPUT);
-    }
+  private void debug(String message) {
+    this.consoleView.print(message + "\n", ConsoleViewContentType.LOG_DEBUG_OUTPUT);
+  }
 }

5dcd9a39509a75c23d181494784e7b3aef2037d0
Amol Grover <29180487+frextrite@users.noreply.github.com>
2019-08-08 16:20:41 +0530

    IssueManager.java: Add class to manage issues

---
+++ src/main/java/io/coala/jetbrains/highlight/IssueManager.java
@@ -0,0 +1,14 @@
+package io.coala.jetbrains.highlight;
+
+import com.intellij.openapi.components.ProjectComponent;
+import com.intellij.openapi.project.Project;
+
+public class IssueManager implements ProjectComponent {
+  
+  private final Project project;
+
+  public IssueManager(Project project) {
+    this.project = project;
+  }
+
+}

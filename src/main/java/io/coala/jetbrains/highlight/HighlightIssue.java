c972c135894a381b0a7e30dc9ca9d86a70ee134f
Amol Grover <29180487+frextrite@users.noreply.github.com>
2019-08-08 16:32:16 +0530

    HighlightIssue.java: Add method to get Text Range directly

--- src/main/java/io/coala/jetbrains/highlight/HighlightIssue.java
+++ src/main/java/io/coala/jetbrains/highlight/HighlightIssue.java
@@ -3,7 +3,7 @@ package io.coala.jetbrains.highlight;
 import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.RangeMarker;
 import com.intellij.openapi.project.Project;
-import com.intellij.psi.PsiFile;
+import com.intellij.openapi.util.TextRange;
 import io.coala.jetbrains.utils.CodeAnalysisIssue.IssueSeverity;
 
 public class HighlightIssue {
@@ -12,27 +12,25 @@ public class HighlightIssue {
   private final String origin;
   private final String message;
   private final IssueSeverity severity;
-  private final RangeMarker range;
+  private final RangeMarker rangeMarker;
 
   private final Project myProject;
   private final Document document;
-  private final PsiFile psiFile;
 
   public HighlightIssue(String id, String origin, String message, IssueSeverity severity,
-      RangeMarker range, Project myProject, Document document, PsiFile psiFile) {
+      RangeMarker rangeMarker, Project myProject, Document document) {
     this.id = id;
     this.origin = origin;
     this.message = message;
     this.severity = severity;
-    this.range = range;
+    this.rangeMarker = rangeMarker;
     this.myProject = myProject;
     this.document = document;
-    this.psiFile = psiFile;
   }
 
   public HighlightIssue(String origin, String message, IssueSeverity severity,
-      RangeMarker range, Project myProject, Document document, PsiFile psiFile) {
-    this(null, origin, message, severity, range, myProject, document, psiFile);
+      RangeMarker rangeMarker, Project myProject, Document document) {
+    this(null, origin, message, severity, rangeMarker, myProject, document);
   }
 
   public String getId() {
@@ -51,8 +49,8 @@ public class HighlightIssue {
     return severity;
   }
 
-  public RangeMarker getRange() {
-    return range;
+  public RangeMarker getRangeMarker() {
+    return rangeMarker;
   }
 
   public Project getMyProject() {
@@ -63,7 +61,15 @@ public class HighlightIssue {
     return document;
   }
 
-  public PsiFile getPsiFile() {
-    return psiFile;
+  public TextRange getTextRange() {
+    return new TextRange(rangeMarker.getStartOffset(), rangeMarker.getEndOffset());
+  }
+
+  public int getStartOffset() {
+    return rangeMarker.getStartOffset();
+  }
+
+  public int getEndOffset() {
+    return rangeMarker.getEndOffset();
   }
 }

fe0557653ed75ddbde7b88198ed98cdb02c76dc9
Amol Grover <29180487+frextrite@users.noreply.github.com>
2019-08-08 16:22:59 +0530

    IssueManager.java: Add methods to create range markers

--- src/main/java/io/coala/jetbrains/highlight/IssueManager.java
+++ src/main/java/io/coala/jetbrains/highlight/IssueManager.java
@@ -1,14 +1,127 @@
 package io.coala.jetbrains.highlight;
 
 import com.intellij.openapi.components.ProjectComponent;
+import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.openapi.editor.Document;
+import com.intellij.openapi.editor.RangeMarker;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.TextRange;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.openapi.vfs.VirtualFileManager;
+import com.intellij.psi.PsiDocumentManager;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.PsiManager;
+import io.coala.jetbrains.utils.AffectedCode;
+import io.coala.jetbrains.utils.CodeAnalysisIssue;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.jetbrains.annotations.NotNull;
 
 public class IssueManager implements ProjectComponent {
-  
-  private final Project project;
 
-  public IssueManager(Project project) {
+  private static final Logger LOGGER = Logger.getInstance(IssueManager.class);
+
+  private final Project project;
+  private final PsiDocumentManager documentManager;
+
+  public IssueManager(Project project, PsiDocumentManager documentManager) {
     this.project = project;
+    this.documentManager = documentManager;
+  }
+
+  public Map<Document, Collection<RangeMarker>> getAllRangeMarkers(
+      List<CodeAnalysisIssue> issueList) throws FileNotFoundException {
+    final Map<Document, Collection<RangeMarker>> rangeMarkers = new HashMap<>();
+
+    for (CodeAnalysisIssue issue : issueList) {
+      final Map<Document, Collection<RangeMarker>> rangeMarkerFromIssue =
+          getRangeMarkerFromIssue(issue);
+
+      for (Map.Entry<Document, Collection<RangeMarker>> element : rangeMarkerFromIssue.entrySet()) {
+        final Document document = element.getKey();
+        final Collection<RangeMarker> rangeMarkerCollection = element.getValue();
+
+        if (!rangeMarkers.containsKey(document)) {
+          rangeMarkers.put(document, new ArrayList<>());
+        }
+
+        for (RangeMarker rangeMarker : rangeMarkerCollection) {
+          if (!rangeMarkers.get(document).contains(rangeMarker)) {
+            rangeMarkers.get(document).add(rangeMarker);
+          }
+        }
+      }
+
+    }
+    return rangeMarkers;
+  }
+
+  public Map<Document, Collection<RangeMarker>> getRangeMarkerFromIssue(CodeAnalysisIssue issue)
+      throws FileNotFoundException {
+    Map<Document, Collection<RangeMarker>> rangeMarkers = new HashMap<>();
+
+    for (AffectedCode affectedCode : issue.getAffectedCodeList()) {
+      final String filePath = affectedCode.getFileName();
+      final PsiFile psiFile = getPsiFile(filePath);
+      final Document document = getDocument(psiFile);
+
+      if (!rangeMarkers.containsKey(document)) {
+        rangeMarkers.put(document, new ArrayList<>());
+      }
+
+      rangeMarkers.get(document).add(createRangeMarker(affectedCode, document));
+    }
+
+    return rangeMarkers;
+  }
+
+  private Document getDocument(PsiFile psiFile) {
+  }
+
+  private PsiFile getPsiFile(String filePath) throws FileNotFoundException {
+    final VirtualFile vfsFile = VirtualFileManager.getInstance().findFileByUrl(filePath);
+
+    if (vfsFile == null) {
+      throw new FileNotFoundException(filePath);
+    }
+
+    return PsiManager.getInstance(project).findFile(vfsFile);
+  }
+
+  private RangeMarker createRangeMarker(@NotNull AffectedCode affectedCode,
+      @NotNull Document document) {
+    final TextRange textRange = getIssueTextRange(affectedCode, document);
+
+    return document.createRangeMarker(textRange);
+  }
+
+  private TextRange getIssueTextRange(@NotNull AffectedCode affectedCode,
+      @NotNull Document document) {
+    int startLine = affectedCode.getStartLine() - 1;
+    int endLine = affectedCode.getEndLine() - 1;
+    int startColumn = affectedCode.getStartColumn();
+    int endColumn = affectedCode.getEndColumn();
+
+    if (startColumn == -1 && endColumn == -1) {
+      startColumn = 0;
+      endLine += 1;
+    } else {
+      startColumn -= 1;
+      endColumn -= 1;
+    }
+
+    final int startOffset = getOffsetInDocument(document, startLine, startColumn);
+    final int endOffset = getOffsetInDocument(document, endLine, endColumn);
+
+    return new TextRange(startOffset, endOffset);
+  }
+
+  private int getOffsetInDocument(@NotNull Document document, int line, int column) {
+    return document.getLineStartOffset(line) + column;
   }
 
 }

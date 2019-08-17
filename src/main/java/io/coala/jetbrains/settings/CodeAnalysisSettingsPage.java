ae84c3b465fd466dba4efc3ec091087099113ec6
Amol Grover <29180487+frextrite@users.noreply.github.com>
2019-08-17 11:08:41 +0530

    CodeAnalysisSettingsPage.java: Implement automatic detection of coala

--- src/main/java/io/coala/jetbrains/settings/CodeAnalysisSettingsPage.java
+++ src/main/java/io/coala/jetbrains/settings/CodeAnalysisSettingsPage.java
@@ -1,5 +1,7 @@
 package io.coala.jetbrains.settings;
 
+import com.intellij.execution.ExecutionException;
+import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.fileChooser.FileChooserDescriptorFactory;
 import com.intellij.openapi.options.Configurable;
 import com.intellij.openapi.options.ConfigurationException;
@@ -7,6 +9,9 @@ import com.intellij.openapi.project.Project;
 import com.intellij.ui.TextFieldWithHistory;
 import com.intellij.ui.TextFieldWithHistoryWithBrowseButton;
 import com.intellij.util.ui.SwingHelper;
+import java.io.IOException;
+import java.nio.file.Path;
+import javax.swing.JButton;
 import javax.swing.JComponent;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -16,11 +21,14 @@ import org.jetbrains.annotations.Nullable;
 
 public class CodeAnalysisSettingsPage implements Configurable {
 
+  private static final Logger LOGGER = Logger.getInstance(CodeAnalysisSettingsPage.class);
+
   private final Project project;
 
   private TextFieldWithHistoryWithBrowseButton coalaLocation;
   private JLabel coalaLocationLabel;
   private JPanel panel;
+  private JButton automaticExecRetrievalButton;
 
   public CodeAnalysisSettingsPage(Project project) {
     this.project = project;
@@ -43,7 +51,26 @@ public class CodeAnalysisSettingsPage implements Configurable {
     final String coalaLocationLabelString = "coala Script Location";
     coalaLocationLabel.setText(coalaLocationLabelString);
 
+    final String automaticExecRequirementButtonLabel = "Automatically detect coala installation";
+    automaticExecRetrievalButton.setText(automaticExecRequirementButtonLabel);
+
+    initializeAutomaticExecRetrievalButton();
     initializeTextFieldWithHistoryWithBrowseButton();
+  }
+
+  private void initializeAutomaticExecRetrievalButton() {
+    automaticExecRetrievalButton.addActionListener(e -> {
+      try {
+        final Path coalaPath = getProjectSettings().determineAndSetExecutable("coala");
+
+        if (coalaPath != null) {
+          final String coalaLocationString = coalaPath.toString();
+          this.coalaLocation.setTextAndAddToHistory(coalaLocationString);
+        }
+      } catch (ExecutionException | InterruptedException | IOException ex) {
+        LOGGER.error(ex);
+      }
+    });
   }
 
   private void initializeTextFieldWithHistoryWithBrowseButton() {

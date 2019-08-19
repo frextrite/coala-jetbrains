ea552dd3bb36765bf4e21b0fee86746f21d14e20
Amol Grover <29180487+frextrite@users.noreply.github.com>
2019-08-19 15:28:37 +0530

    HighlightIssueFactory.java: Add javadoc

--- src/main/java/io/coala/jetbrains/highlight/HighlightIssueFactory.java
+++ src/main/java/io/coala/jetbrains/highlight/HighlightIssueFactory.java
@@ -9,14 +9,35 @@ public class HighlightIssueFactory implements ProjectComponent {
 
   private final Map<Document, HighlightIssueWrapper> highlightIssueWrappers = new HashMap<>();
 
+  /**
+   * This method returns an instance of {@link HighlightIssueWrapper}
+   * corresponding to the supplied document.
+   *
+   * @param document the document instance corresponding to which
+   *     HighlightIssueWrapper data is required
+   * @return instance of {@link HighlightIssueWrapper}
+   */
   public HighlightIssueWrapper getComponent(Document document) {
     return highlightIssueWrappers.get(document);
   }
 
+  /**
+   * This method adds an instance of given {@link HighlightIssueWrapper} to the
+   * global map with key as given instance of {@link Document}.
+   *
+   * @param document the document corresponding to the object
+   * @param highlightIssueWrapper the actual object to be stored
+   */
   public void putComponent(Document document, HighlightIssueWrapper highlightIssueWrapper) {
     highlightIssueWrappers.put(document, highlightIssueWrapper);
   }
 
+  /**
+   * This method returns all the {@link HighlightIssueWrapper} instances with
+   * their corresponding documents.
+   *
+   * @return map with keys as {@link Document} and value as {@link HighlightIssueWrapper}
+   */
   public Map<Document, HighlightIssueWrapper> getHighlightIssueWrappers() {
     return this.highlightIssueWrappers;
   }

00947fa213322e51270245c20d0db57a82298060
Amol Grover <29180487+frextrite@users.noreply.github.com>
2019-08-20 15:19:36 +0530

    CodeAnalysisExecutor.java: Add method to save all files

--- src/main/java/io/coala/jetbrains/analysis/CodeAnalysisExecutor.java
+++ src/main/java/io/coala/jetbrains/analysis/CodeAnalysisExecutor.java
@@ -1,6 +1,9 @@
 package io.coala.jetbrains.analysis;
 
+import com.intellij.openapi.application.WriteAction;
 import com.intellij.openapi.components.ProjectComponent;
+import com.intellij.openapi.fileEditor.FileDocumentManager;
+import com.intellij.openapi.progress.ProgressIndicator;
 import com.intellij.openapi.progress.ProgressManager;
 import com.intellij.openapi.progress.impl.BackgroundableProcessIndicator;
 import com.intellij.openapi.project.Project;
@@ -18,9 +21,33 @@ public class CodeAnalysisExecutor implements ProjectComponent {
     this.progressManager = progressManager;
   }
 
+  /**
+   * Runs our custom {@link CodeAnalysisTask} synchronously with the specified progress indicator.
+   *
+   * @param task instance of {@link CodeAnalysisTask} to be run
+   * @see ProgressManager#runProcess(Runnable, ProgressIndicator)
+   */
   public void runTask(CodeAnalysisTask task) {
     BackgroundableProcessIndicator processIndicator = new BackgroundableProcessIndicator(task);
     executor.submit(() ->
         progressManager.runProcess(() -> task.run(processIndicator), processIndicator));
+  }
+
+  /**
+   * Saves all the cached documents to the file system and runs the task.
+   *
+   * <p/>
+   * Documents are needed to be saved since coala can only be run on filesystem files
+   *
+   * @param task instance of {@link CodeAnalysisTask} to be run
+   * @see #runTask(CodeAnalysisTask)
+   */
+  public void saveAllDocumentsAndRunTask(CodeAnalysisTask task) {
+    saveAllDocuments();
+    runTask(task);
+  }
+
+  private void saveAllDocuments() {
+    WriteAction.run(() -> FileDocumentManager.getInstance().saveAllDocuments());
   }
 }
